20b6e5be2c3d6bc156406ec966e3390943d1c765
DeepAQ <5405949+DeepAQ@users.noreply.github.com>
2020-08-06 21:20:35 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Jupyter_PowerShell5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# Jupyter-PowerShell5
+
+Jupyter kernel implementation for Windows PowerShell 5.1
